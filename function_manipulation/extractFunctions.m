6f99444acdc62e1df172a4f807d5f4d59e055d72
Ian McInerney <I.McInerney17@imperial.ac.uk>
2018-02-21 17:58:54 +0000

    Added breakpoint extraction

--- function_manipulation/extractFunctions.m
+++ function_manipulation/extractFunctions.m
@@ -1,4 +1,4 @@
-function [ numFunc, funcs ] = extractFunctions( filename )
+function [ numFunc, funcs ] = extractFunctions( filename, varargin )
 %EXTRACTFUNCTIONS Extract all functions that are within a specified file
 %
 % This function will extract all the functions present in the file and save
@@ -8,13 +8,20 @@ function [ numFunc, funcs ] = extractFunctions( filename )
 % whitespace in front of the function declaration, so it leaves nested
 % functions intact.
 %
+% This function is able to extract the breakpoint data from the file, and
+% then transfer the breakpoints to the newly extracted functions. If
+% brekpoint extraction is not selected, then the file is created with no
+% breakpoints enabled.
+%
 % 
 % Usage:
-%   [ numFunc ] = extractFunctions( filename );
 %   [ numFunc, funcs ] = extractFunctions( filename );
+%   [ numFunc, funcs ] = extractFunctions( filename, ExtractBreakpoints );
 %
 % Inputs:
 %   filename - The name of the file to parse
+%   ExtractBreakpoints - Flag to extract breakpoints (0=don't extrac,
+%                        1=extract). Defaults to not extracting breakpoints.
 %
 % Outputs:
 %   numFunc - The number of functions extracted from the file
@@ -24,11 +31,20 @@ function [ numFunc, funcs ] = extractFunctions( filename )
 %
 % Created by: Ian McInerney
 % Created on: February 20, 2018
-% Version: 1.0
-% Last Modified: February 20, 2018
+% Version: 1.1
+% Last Modified: February 21, 2018
 %
 % Revision History
 %   1.0 - Initial release
+%   1.1 - Added breakpoint extraction
+
+
+%% Parse the optional input
+p = inputParser;
+addOptional(p, 'ExtractBreakpoints', 0);
+
+parse(p, varargin{:});
+eb = p.Results.ExtractBreakpoints;
 
 
 %% Open the file
@@ -43,6 +59,9 @@ end
 fw = -1;
 numFunc = 0;
 funcs = {};
+files = {};
+lnum = 1;     % The line number
+fLine = [1, 1];
 
 
 %% Loop while not end of file
@@ -53,13 +72,18 @@ while ( ~feof(fr) )
     
     % The line defines a function
     if (~isempty( isFunc ) )
+        
         % Close the current function file
         if (fw ~= -1)
+            fLine(numFunc, 2) = lnum-1;
             fclose(fw);
         end
         
         % Increment the counter
         numFunc = numFunc + 1;
+        
+        % Save the starting line
+        fLine(numFunc, 1) = lnum;
         
         % Extract the function name
         fname = regexpi(l, '[\S]*\(', 'match');
@@ -71,6 +95,7 @@ while ( ~feof(fr) )
         
         % Make the file name
         fname = [fname, '.m'];
+        files{numFunc} = fname;
         
         % Open the file
         fw = fopen(fname, 'w');
@@ -83,17 +108,62 @@ while ( ~feof(fr) )
         end
     end
     
+    lnum = lnum + 1;
+    
     % Write to the function file if it is open
     if (fw ~= -1)
         fprintf(fw, '%s', l);
     end
 end
 
+% Add the last line
+fLine(numFunc, 2) = lnum;
+
 
 %% Close the files that are open
 fclose(fr);
 if (fw ~= -1)
     fclose(fw);
+end
+
+
+%% Force MATLAB to find the new functions
+rehash
+
+
+%% Clear the debug information for the files that have been extracted
+for ( i=1:1:numFunc )
+    eval(['dbclear ', files{i}]);
+end
+
+
+%% Extract debug information if requested
+if (eb)
+    
+    % Get the breakpoint information for the original file
+    bp = dbstatus(filename);
+    numBP = length(bp);
+    
+    % Iterate through each breakpoint
+    for ( i=1:1:numBP )
+        % Figure out which function it goes with
+        condMet = (bp(i).line >= fLine(:,1)) + (bp(i).line <= fLine(:,2));
+        fInd = find( condMet == 2 );
+        
+        lNum = bp(i).line - fLine(fInd,1) + 1;
+        
+        % Extract the expression if it is there
+        expre = bp(i).expression{:};
+        
+        % Set the new breakpoint
+        if ( isempty(expre) )
+            eval(['dbstop in ', files{fInd}, ' at ', num2str(lNum), ';']);
+        else
+            eval(['dbstop in ', files{fInd}, ' at ', num2str(lNum), ' if ''', expre, ''';']);
+        end
+        
+    end
+    
 end
 
 end
